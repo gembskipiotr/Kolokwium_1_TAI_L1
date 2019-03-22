6c902658d11dd0d2d7d46254fb1f541bd9cbe3a3
gembskipiotr <43858230+gembskipiotr@users.noreply.github.com>
2019-03-22 16:42:31 +0100

    Add files via upload

---
+++ navbar.js
@@ -0,0 +1,5 @@
+let nav = '<nav><h1 class="text">Nawigacja</h1>' +
+'<a class="text" href="./index.html">Home</a>' +
+'<a class="text" href="./kontakt.html">Kontakt</a> </nav>';
+let elem = document.querySelector(".nav-menu");
+elem.innerHTML = nav;
